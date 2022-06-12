09f74d5428d648f24b23c4e3485f5f16a848a0ac
lxlongKunkka <110076790@qq.com>
2022-06-12 17:53:17 +0800

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,23 +1,25 @@
+basic.showIcon(IconNames.Happy)
 radio.setGroup(1)
 basic.forever(function () {
     ThinkJoyStick.ListenKeyPad()
     if (ThinkJoyStick.ReadKeyPad(KEY.UP)) {
-
+        radio.sendString("forward")
         basic.showArrow(ArrowNames.North)
     }
     else if (ThinkJoyStick.ReadKeyPad(KEY.DOWN)) {
-
+        radio.sendString("back")
         basic.showArrow(ArrowNames.South)
     }
     else if (ThinkJoyStick.ReadKeyPad(KEY.LEFT)) {
-
+        radio.sendString("left")
         basic.showArrow(ArrowNames.West)
     }
     else if (ThinkJoyStick.ReadKeyPad(KEY.RIGHT)) {
-
+        radio.sendString("right")
         basic.showArrow(ArrowNames.East)
     }
     else if (ThinkJoyStick.ReadKeyPad(KEY.NONE)) {
-        basic.showIcon(IconNames.Heart)
+        radio.sendString("stop")
+        basic.showIcon(IconNames.SmallHeart)
     }
 })
