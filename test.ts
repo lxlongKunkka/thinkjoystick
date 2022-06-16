759044192c76864bbc6b2c7f1cbc91df1e40ea9b
lxlongKunkka <110076790@qq.com>
2022-06-16 15:24:54 +0800

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,9 @@
 basic.showIcon(IconNames.Happy)
 radio.setGroup(1)
+let strip = neopixel.create(DigitalPin.P3, 4, NeoPixelMode.RGB)
+strip.setBrightness(5)
+strip.showRainbow()
+
 basic.forever(function () {
     ThinkJoyStick.ListenKeyPad()
     if (ThinkJoyStick.ReadKeyPad(KEY.UP)) {
@@ -21,5 +25,29 @@ basic.forever(function () {
     else if (ThinkJoyStick.ReadKeyPad(KEY.NONE)) {
         radio.sendString("stop")
         basic.showIcon(IconNames.SmallHeart)
+    }
+    if (ThinkJoyStick.ReadKeyPad(KEY.A))
+    {
+        strip.rotate()
+        strip.show()
+        basic.showString("A")
+    }
+    else if (ThinkJoyStick.ReadKeyPad(KEY.B))
+    {
+        strip.showBarGraph(0, 255)
+        strip.show()
+        basic.showString("B")
+    }
+    else if (ThinkJoyStick.ReadKeyPad(KEY.X)) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+        basic.showString("X")
+    }
+    else if (ThinkJoyStick.ReadKeyPad(KEY.Y))
+    {
+        strip.showRainbow()
+        strip.clear()
+        strip.show()
+        basic.showString("Y")
     }
 })
