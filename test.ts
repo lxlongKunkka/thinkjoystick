f056aed24b4bf5d94c005868edb08b9aa155b7d1
lxlongKunkka <110076790@qq.com>
2022-06-17 17:52:29 +0800

    更新 test.ts

--- test.ts
+++ test.ts
@@ -3,6 +3,74 @@ radio.setGroup(1)
 let strip = neopixel.create(DigitalPin.P3, 4, NeoPixelMode.RGB)
 strip.setBrightness(5)
 strip.showRainbow()
+let tempnow = 0
+ThinkJoyStick.init(60)
+let index = 0
+ThinkJoyStick.showString(
+    0,
+    0,
+    "Hello!",
+    1
+)
+let min = input.temperature()
+let max = input.temperature()
+basic.forever(function () {
+    tempnow = input.temperature()
+    if (tempnow > max) {
+        max = tempnow
+    }
+    if (tempnow < min) {
+        min = tempnow
+    }
+    ThinkJoyStick.showNumber(
+        5,
+        0,
+        tempnow,
+        1
+    )
+    ThinkJoyStick.pixel(index, 32 - Math.map(tempnow, 15, 35, 0, 20), 1)
+    ThinkJoyStick.draw()
+    index += 1
+    if (index == 64) {
+        index = 0
+        ThinkJoyStick.clear()
+        ThinkJoyStick.showString(
+            0,
+            0,
+            "Temp",
+            1
+        )
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        ThinkJoyStick.showString(
+            0,
+            3,
+            "Min",
+            1
+        )
+        ThinkJoyStick.showNumber(
+            5,
+            3,
+            min,
+            1
+        )
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        ThinkJoyStick.showString(
+            0,
+            3,
+            "Min",
+            1
+        )
+        ThinkJoyStick.showNumber(
+            5,
+            3,
+            max,
+            1
+        )
+    }
+})
+
 
 basic.forever(function () {
     ThinkJoyStick.ListenKeyPad()
@@ -34,12 +102,12 @@ basic.forever(function () {
     }
     else if (ThinkJoyStick.ReadKeyPad(KEY.B))
     {
-        strip.showBarGraph(0, 255)
+        strip.shift()
         strip.show()
         basic.showString("B")
     }
     else if (ThinkJoyStick.ReadKeyPad(KEY.X)) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+        strip.showRainbow()
         strip.show()
         basic.showString("X")
     }
